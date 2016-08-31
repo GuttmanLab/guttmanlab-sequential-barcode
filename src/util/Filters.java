a0a5112b61412cc5ef86a603a08f5d9238fa1e3d
pamelarussell <pamela.russell@gmail.com>
2016-08-31 05:23:24 -0600

    Molecule type predicates for SAMRecords and Avro records

--- src/util/Filters.java
+++ src/util/Filters.java
@@ -33,6 +33,64 @@ public class Filters {
 		return attribute.equals("T");
 	};
 	
+	private static void validateMoleculeTagVal(String tagVal) {
+		if(!tagVal.equals(CustomSamTag.RNA_DNA_VAL_DNA) && !tagVal.equals(CustomSamTag.RNA_DNA_VAL_RNA)) {
+			throw new IllegalArgumentException("Illegal tag value: " + tagVal + ". Options: " + CustomSamTag.RNA_DNA_VAL_DNA + " " + CustomSamTag.RNA_DNA_VAL_RNA);
+		}
+	}
+	
+	/**
+	 * Get a predicate to test whether a record is marked as RNA or DNA
+	 * @param tagVal Value of tag to test for. Allowable options: RNA value or DNA value
+	 * @return The predicate
+	 */
+	private static final Predicate<SAMRecord> moleculeTypeReadPredicate(String tagVal) {
+		validateMoleculeTagVal(tagVal);
+		return record -> {
+			String attribute = record.getStringAttribute(CustomSamTag.RNA_DNA);
+			if(attribute == null) {
+				throw new IllegalArgumentException("Record does not have " + CustomSamTag.RNA_DNA + " tag: " + record.toString());
+			}
+			return attribute.equals(tagVal);
+		};
+	}
+	
+	/**
+	 * Get a predicate to test whether a record is marked as RNA or DNA
+	 * @param tagVal Value of tag to test for. Allowable options: RNA value or DNA value
+	 * @return The predicate
+	 */
+	private static final Predicate<AvroSamRecord> moleculeTypeAvroPredicate(String tagVal) {
+		validateMoleculeTagVal(tagVal);
+		return record -> {
+			String attribute = record.getAttribute(CustomSamTag.RNA_DNA).toString();
+			if(attribute == null) {
+				throw new IllegalArgumentException("Record does not have " + CustomSamTag.RNA_DNA + " tag: " + record.toString());
+			}
+			return attribute.equals(tagVal);
+		};
+	}
+	
+	/**
+	 * Test whether a record is marked as RNA
+	 */
+	public static final Predicate<SAMRecord> readIsRNA = moleculeTypeReadPredicate(CustomSamTag.RNA_DNA_VAL_RNA);
+	
+	/**
+	 * Test whether a record is marked as DNA
+	 */
+	public static final Predicate<SAMRecord> readIsDNA = moleculeTypeReadPredicate(CustomSamTag.RNA_DNA_VAL_DNA);
+	
+	/**
+	 * Test whether a record is marked as RNA
+	 */
+	public static final Predicate<AvroSamRecord> recordIsRNA = moleculeTypeAvroPredicate(CustomSamTag.RNA_DNA_VAL_RNA);
+	
+	/**
+	 * Test whether a record is marked as DNA
+	 */
+	public static final Predicate<AvroSamRecord> recordIsDNA = moleculeTypeAvroPredicate(CustomSamTag.RNA_DNA_VAL_DNA);
+	
 	/**
 	 * Get the default avro record filters
 	 * @return Default avro record filters
