d57337870279eefa4bfde8b7b1c24145694744a4
pamelarussell <pamela.russell@gmail.com>
2016-08-31 05:22:37 -0600

    Add molecule type custom tag ('XX')

--- src/util/CustomSamTag.java
+++ src/util/CustomSamTag.java
@@ -14,6 +14,22 @@ public class CustomSamTag {
 	public static final String HAS_ALL_BARCODES = "XA";
 	public static final String RRNA_ALIGNED = "XR";
 	public static final String CONTAINED_IN_ANNOTATION = "XF";
+	
+	/**
+	 * Tag indicating whether the read originated from RNA or DNA
+	 */
+	public static final String RNA_DNA = "XX";
+	
+	/**
+	 * Value of the RNA/DNA tag indicating RNA
+	 */
+	public static final String RNA_DNA_VAL_RNA = "RNA";
+	
+	/**
+	 * Value of the RNA/DNA tag indicating DNA
+	 */
+	public static final String RNA_DNA_VAL_DNA = "DNA";
+	
 	/**
 	 * Get the transcript IDs listed in a transcript overlapper tag value
 	 * @param transcriptOverlapperTagValue Tag value to parse
